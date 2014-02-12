f686971a1450b6f6d4c69e2f9d2c7008204a6f2f
Kent Ma <tnekent@gmail.com>
2014-02-12 08:38:50 -0500

    HI

---
+++ src/sketch.ino
@@ -0,0 +1,15 @@
+/*
+ * Mission Submersible
+ * ~~~~~~~~~~~~~~~~~~~
+ *
+ * Author: Diwakar Ganesan, Kent Ma, Trishul Nagenalli
+ */
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Hello world!");
+}
+
+void loop()
+{
+}
